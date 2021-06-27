fb42331dcd779c39ee105e13231ba8355710602f
fde-capu <flavio@carrara.us>
2021-06-27 16:36:33 -0300

    very usefull git commig message

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: fde-capu </var/mail/fde-capu>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:50:26 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/06/25 11:31:24 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/06/27 14:31:32 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 # include <iostream>
 # include <cstddef>
 
-template<typename T, typename F>
-void iter(T * array, size_t array_length, F & fun)
 /*
 ** It seems is useless to have the same function running for each element if
 ** the function does not use the array value to process anything (it seems but
@@ -25,6 +23,8 @@ void iter(T * array, size_t array_length, F & fun)
 ** in this case, fun() without arguments, let's loop fun() array_length times.
 ** It is not mentined that after running the loop the array would be modified.
 */
+template<typename T, typename F>
+void iter(T * array, size_t array_length, F fun)
 {
 	(void)array;
 	for (size_t i = 0; i < array_length; i++)
@@ -32,30 +32,45 @@ void iter(T * array, size_t array_length, F & fun)
 	return ;
 }
 
-template<typename T, typename F>
-void iter(T * array, size_t array_length, F fun(T))
 /*
 ** This works if the argument of fun is of the same type as the array.
 ** So the fun is called each with each array value as argument (seems logical).
 ** However, it is not mentioned that the function should be able
 ** to modify the array values.
 */
+template<typename T, typename F>
+void iter(T * array, size_t array_length, F fun(T))
 {
 	for (size_t i = 0; i < array_length; i++)
 		fun(array[i]);
 	return ;
 }
 
-template<typename T, typename F>
-void iter(T * array, size_t array_length, F fun(T &))
 /*
 ** This function is called whenever the passed function "fun" has one argument
 ** consisting of a referente to "something" of the same type of the array.
 ** This also does not modify the original array, for this was not mentioned.
 */
+template<typename T, typename F>
+void iter(T * array, size_t array_length, F fun(T &))
 {
 	for (size_t i = 0; i < array_length; i++)
 		fun(array[i]);
+	return ;
+}
+
+/*
+** Now, if the function gets as argument something else
+** than the same type of array. Seems useless (but might not be)
+** since does not takt array[i] value into consideration.
+*/
+template<typename T, typename F, typename FT>
+void iter(T * array, size_t array_length, F fun(FT &))
+{
+	(void)array;
+	FT foo = 42;
+	for (size_t i = 0; i < array_length; i++)
+		fun(foo);
 	return ;
 }
 
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fde-capu </var/mail/fde-capu>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 10:50:08 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/06/24 18:40:01 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/06/27 14:39:22 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,35 @@
 #include <sstream>
 #include <iomanip>
 
-void some_function_without_arguments()
+void void_function_no_args()
 {
-	std::cout << "some_function_without_arguments() called." << std::endl;
+	std::cout << "void_function_no_args() called." << std::endl;
+	return ;
+}
+
+int int_function_no_args()
+{
+	std::cout << "int_function_no_args() called." << std::endl;
+	return 42;
+}
+
+template<typename T>
+void void_function_argtype_equals(T x)
+{
+	std::cout << "void_function_argtype_equals(" << x << ") called." << std::endl;
 	return ;
 }
 
 template<typename T>
-void some_function_with_one_arg(T x)
+void void_function_argtype_ref_to_type_equals(T & x)
 {
-	std::cout << "some_function_with_one_arg(" << x << ") called." << std::endl;
-	return ;
-}
-
-template<typename T>
-void some_function_with_one_ref(T & x)
-{
-	std::cout << "some_function_with_one_ref(" << x << ") called." << std::endl;
+	std::cout << "void_function_argtype_ref_to_type_equals(" << x << ") called." << std::endl;
 	return ;
 }
 
 template<typename T>
 void hex_dump(T & block)
 {
-	std::cout << "hex_dump(" << block << ") called." << std::endl;
 	char * byte;
 	std::size_t address = reinterpret_cast<std::size_t>(&block);
 	std::stringstream dump;
@@ -57,29 +62,73 @@ void hex_dump(T & block)
 	return ;
 }
 
+template<typename T>
+void test(T * array, size_t array_length, std::string title)
+{
+	std::cout << title << std::endl << "---" << std::endl;;
+	::iter(array, array_length, void_function_no_args);
+	std::cout << "---" << std::endl;
+	::iter(array, array_length, int_function_no_args);
+	std::cout << "---" << std::endl;
+	::iter(array, array_length, void_function_argtype_equals<T>);
+	std::cout << "---" << std::endl;
+	::iter(array, array_length, void_function_argtype_ref_to_type_equals<T>);
+	std::cout << "---" << std::endl;
+	::iter(array, array_length, hex_dump<T>);
+	std::cout << "---" << std::endl;
+	::iter(array, array_length, int_function_no_args);
+	std::cout << "===" << std::endl;
+	std::cout << std::endl;
+}
+
+void f_a() { std::cout << "function a" << std::endl; }
+void f_b() { std::cout << "function b" << std::endl; }
+void f_c() { std::cout << "function c" << std::endl; }
+
 int main()
 {
-	size_t array_length = 4;
-	int array[array_length];
-
-	array[0] = 42;
-	array[1] = 142;
-	array[2] = 123;
-	array[3] = -42;
-
-	std::cout << std::endl << "::iter(array, array_length, some_function_without_arguments);" << std::endl;
-	::iter(array, array_length, some_function_without_arguments);
-
-	std::cout << std::endl << "::iter(array, array_length, some_function_with_one_arg<int>);" << std::endl;
-	::iter(array, array_length, some_function_with_one_arg<int>); // <int> makes it instantiated.
-//	::iter(array[0], array_length, some_function_with_one_arg); // Cannot infer template argument.
-
-	std::cout << std::endl << "::iter(array, array_length, some_function_with_one_ref<int>);" << std::endl;
-	::iter(array, array_length, some_function_with_one_ref<int>); // <int> is same type of array.
-//	::iter(array[0], array_length, some_function_with_one_ref<char>); // Not prototyped. Function argument must be of same type as the array.
-
-	std::cout << std::endl << "::iter(array, array_length, hex_dump<int>);" << std::endl;
-	::iter(array, array_length, hex_dump<int>);
-
+	{
+		const size_t array_length = 4;
+		int array[array_length] = {42, 142, 123, -42};
+		test(array, array_length, "int array[4]");
+	}
+	{
+		const size_t array_length = 4;
+		int array[array_length] = {42, 142, 123, -42};
+		test(array, array_length, "int array[4]");
+	}
+	{
+		const size_t array_length = 5;
+		std::string array[array_length] = {"ABC", "forty two", "anything", "", "0"};
+		test(array, array_length, "std::string array[5]");
+	}
+	{
+		int arr_a[3] = {8, 1, 6};
+		int arr_b[3] = {3, 5, 7};
+		int arr_c[3] = {4, 9, 2};
+		const size_t array_length = 3;
+		int * array[array_length] = {arr_a, arr_b, arr_c};
+		test(array, array_length, "Array of arrays");
+	}
+	{
+		const size_t array_length = 3;
+		void (*array[array_length])() = {f_a, f_b, f_c};
+		test(array, array_length, "Array of void functions");
+	}
+	{
+		std::cout << "More tests" << std::endl << "---" << std::endl;
+		const size_t array_length = 4;
+		int array[array_length] = {42, 142, 123, -42};
+		// For sake of explanation: for templated functions:
+		// ::iter(array, array_length, hex_dump); // This doesn't work.
+		::iter(array, array_length, hex_dump<int>); // This is instantiated.
+		std::cout << "---" << std::endl;
+		// In the following case, the function is instantiated
+		// with another type than array is.
+		::iter(array, array_length, hex_dump<char>);
+		std::cout << "---" << std::endl;
+		::iter(array, array_length, void_function_argtype_ref_to_type_equals<char>);
+//		::iter(array, array_length, void_function_argtype_equals<char>);
+	}
 	return 0;
 }
