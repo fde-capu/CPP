9659c621afed09c4870f21e7347a5305c0f6ad53
Flavio Carrara De Capua <flavio@carrara.us>
2021-07-28 16:01:44 -0300

    07ex00!

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -6,16 +6,14 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 17:06:59 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/07/28 14:48:07 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/07/28 15:09:14 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <iostream>
 # include <algorithm>
-# include <list>
 
 template<typename T>
 typename T::iterator easyfind(T & int_container, int needle)
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 17:07:54 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/07/28 14:47:27 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/07/28 16:00:34 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,87 @@
 
 #include <iostream>
 #include <algorithm>
-#include <list>
 #include <iterator>
-#include <vector>
 #include <string>
+#include <list>
+#include <vector>
+#include <deque>
 
 int main ()
 {
+	std::cout << std::endl << "[ A ] std::list<int>" << std::endl;
+
 	std::list<int> list;
+	std::list<int>::iterator list_it;
+
 	list.push_back(7);
 	list.push_back(17);
 	list.push_back(42);
 	list.push_back(-117);
 
-	std::list<int>::iterator head_it;
-	
-	head_it = ::easyfind(list, 42);
-//	head_it = std::find(list.begin(), list.end(), 42);
-	std::cout << "Finds 42 >> " << *head_it << std::endl;
-	if (head_it != list.end())
+	list_it = ::easyfind(list, 42);
+	std::cout << "Finds 42 >> " << *list_it << std::endl;
+	if (list_it != list.end())
 		std::cout << "Found." << std::endl;
 	else
 		std::cout << "Not found." << std::endl;
 
-	head_it = ::easyfind(list, 8);
-//	head_it = std::find(list.begin(), list.end(), 8);
-	std::cout << "Finds 42 >> " << *head_it << std::endl;
-	if (head_it != list.end())
+	list_it = ::easyfind(list, 8);
+	std::cout << "Finds 8 >> " << *list_it << std::endl;
+	if (list_it != list.end())
 		std::cout << "Found." << std::endl;
 	else
 		std::cout << "Not found." << std::endl;
 
 
-//	head_it = ::easyfind(list, 8);
-//	std::cout << "Finds >> " << *head_it << " << is (size) past the last element:" << std::endl;
-//	std::cout << "list.end() " << *list.end() << std::endl;
-//	if (head_it)
-//		std::cout << "Found." << std::endl;
-//	else
-//		std::cout << "Not found." << std::endl;
+	std::cout << std::endl << "[ B ] std::vector<int>" << std::endl;
+
+	std::vector<int> vector;
+	std::vector<int>::iterator vector_it;
+
+	vector.push_back(7);
+	vector.push_back(17);
+	vector.push_back(42);
+	vector.push_back(-117);
+
+	vector_it = ::easyfind(vector, 42);
+	std::cout << "Finds 42 >> " << *vector_it << std::endl;
+	if (vector_it != vector.end())
+		std::cout << "Found." << std::endl;
+	else
+		std::cout << "Not found." << std::endl;
+
+	vector_it = ::easyfind(vector, 8);
+	std::cout << "Finds 8 >> " << *vector_it << std::endl;
+	if (vector_it != vector.end())
+		std::cout << "Found." << std::endl;
+	else
+		std::cout << "Not found." << std::endl;
+
+
+	std::cout << std::endl << "[ C ] std::deque<int>" << std::endl;
+
+	std::deque<int> deque;
+	std::deque<int>::iterator deque_it;
+
+	deque.push_back(7);
+	deque.push_back(17);
+	deque.push_back(42);
+	deque.push_back(-117);
+
+	deque_it = ::easyfind(deque, 42);
+	std::cout << "Finds 42 >> " << *deque_it << std::endl;
+	if (deque_it != deque.end())
+		std::cout << "Found." << std::endl;
+	else
+		std::cout << "Not found." << std::endl;
+
+	deque_it = ::easyfind(deque, 8);
+	std::cout << "Finds 8 >> " << *deque_it << std::endl;
+	if (deque_it != deque.end())
+		std::cout << "Found." << std::endl;
+	else
+		std::cout << "Not found." << std::endl;
 
 	return 0;
 }
