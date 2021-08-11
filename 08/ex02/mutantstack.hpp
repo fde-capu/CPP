9c63599a04f760f615388bd124964906a2f06d66
Flavio Carrara De Capua <flavio@carrara.us>
2021-08-11 16:43:51 -0300

    08 redux

--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -6,32 +6,22 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 09:08:27 by fde-capu          #+#    #+#             */
-/*   Updated: 2021/08/11 16:33:01 by fde-capu         ###   ########.fr       */
+/*   Updated: 2021/08/11 16:43:37 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-# include <iostream>
 # include <stack>
 
 template<typename T>
 class MutantStack: public std::stack<T>
 {
-	private:
 	public:
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin() { return this->c.begin(); }
 		iterator end() { return this->c.end(); }
 };
-
-template<typename T>
-std::ostream & operator<< (std::ostream & o, MutantStack<T> const & self)
-{
-	o << "::MutantStack::" << std::endl;
-	(void)self;
-	return o;
-}
 
 #endif
